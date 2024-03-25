8548d40625de081ee8ad743cddb2d416e6d37de8
gitover22 <qidizou88@gmail.com>
2024-03-25 20:04:21 +0800

    rand test

--- test.go
+++ test.go
@@ -1,15 +1,15 @@
 package main
 
-import "fmt"
-
-func add(x, y int) int { // 当连续两个或多个函数的已命名形参类型相同时，可省略形参
-	return x + y
-}
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	// rand.Seed(time.Now().UnixNano())
-	// fmt.Printf("%s\n", "string output")
-	// fmt.Println("random:", rand.Intn(100))
-	// fmt.Println(math.Pi) // 大写字母开头的就是已导出的
-	fmt.Println("add(10,20):", add(10, 20))
+	rand.Seed(time.Now().UnixNano()) // 播种
+	fmt.Printf("%s\n", "string output")
+	fmt.Println("random:", rand.Intn(100)) // 取随机数[0,100)
+	fmt.Println(math.Pi)                   // 大写字母开头的就是已导出的
 }
