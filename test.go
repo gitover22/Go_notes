30fc2c614a6f3967b10bae358c3097ec4c34d109
gitover22 <qidizou88@gmail.com>
2024-03-26 10:10:58 +0800

    零值

--- test.go
+++ test.go
@@ -2,18 +2,12 @@ package main
 
 import (
 	"fmt"
-	"math/cmplx"
-)
-
-var (
-	tobe    bool       = true
-	MAX_INT uint64     = 1<<64 - 1
-	z       complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
-	fmt.Printf("type: %T ,value:%v\n", tobe, tobe)
-	fmt.Printf("type: %T ,value:%v\n", MAX_INT, MAX_INT)
-	fmt.Printf("type: %T ,value:%v\n", z, z)
-
+	var i int
+	var j bool
+	var f float32
+	var s string
+	fmt.Printf("%v,%v,%v,%q\n", i, j, f, s)
 }
