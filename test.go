c907887ac8fda6bee27f573e768511c4e22e1d1a
gitover22 <qidizou88@gmail.com>
2024-03-26 19:08:43 +0800

    Stringer test

--- test.go
+++ test.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-func do(i interface{}) {
-	switch v := i.(type) {
-	case int:
-		fmt.Printf("Twice %v is %v\n", v, v*2)
-	case string:
-		fmt.Printf("%q is %v bytes long\n", v, len(v))
-	default:
-		fmt.Printf("I don't know about type %T!\n", v)
-	}
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	fmt.Println("String")
+	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
 }
 
 func main() {
-	do(21)
-	do("hello")
-	do(true)
+	a := Person{"Arthur Dent", 42}
+	z := Person{"Zaphod Beeblebrox", 9001}
+	// fmt.Println(a.String())
+	fmt.Println(a, z)
 }
