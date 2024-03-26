f7b8ae59c49105b4f9e3802a8f48f6a39c1b40d3
gitover22 <qidizou88@gmail.com>
2024-03-26 10:20:50 +0800

    变量自动类型推导

--- test.go
+++ test.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	// 自动类型推导
 	i := 10
-	var f float32 = float32(i)
-	var d float64 = float64(f)
-	var b bool = bool(true)
-	fmt.Printf("%v,  %v, %v, %v\n", i, f, d, b)
+	f := 3.1415926
+	c := 1 + 2i
+	t := false
+	// 显示说明
+	var ii int = 12
+	fmt.Printf("type: %T,  %T, %T, %T,%T\n", i, f, c, t, ii)
 }
