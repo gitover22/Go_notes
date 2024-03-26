122e3e2accce7c4dbbc272fcc233c49f2b8546d6
gitover22 <qidizou88@gmail.com>
2024-03-26 16:21:47 +0800

    闭包

--- test.go
+++ test.go
@@ -2,19 +2,23 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func compute(fn func(float64, float64) float64) float64 {
-	return fn(3, 4)
+// 闭包的用法示例
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
 
 func main() {
-	hypot := func(x, y float64) float64 {
-		return math.Sqrt(x*x + y*y)
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
 	}
-	fmt.Println(hypot(5, 12))
-
-	fmt.Println(compute(hypot))
-	fmt.Println(compute(math.Pow))
 }
