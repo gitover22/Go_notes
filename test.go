107ff7d91faf555a4d2a4e1918129655c649ca21
gitover22 <qidizou88@gmail.com>
2024-03-26 18:45:08 +0800

    接口值nil测试

--- test.go
+++ test.go
@@ -2,40 +2,33 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-// 定义接口 I
 type I interface {
-	M() // 接口包含的方法
+	M()
 }
 
-// 类型T
 type T struct {
 	S string
 }
 
-// 类型T的指针型方法M
 func (t *T) M() {
+	if t == nil {
+		fmt.Println("<nil>")
+		return
+	}
 	fmt.Println(t.S)
-}
-
-// 类型F
-type F float64
-
-// 类型F的值类型方法M
-func (f F) M() {
-	fmt.Println(f)
 }
 
 func main() {
 	var i I
 
-	i = &T{"Hello"}
+	var t *T
+	i = t
 	describe(i)
 	i.M()
 
-	i = F(math.Pi)
+	i = &T{"hello"}
 	describe(i)
 	i.M()
 }
