3869e356b98f7e31ece34e96eca6c69aaf499b06
gitover22 <qidizou88@gmail.com>
2024-03-26 10:16:22 +0800

    go要求只能显式类型转化

--- test.go
+++ test.go
@@ -1,13 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var i int
-	var j bool
-	var f float32
-	var s string
-	fmt.Printf("%v,%v,%v,%q\n", i, j, f, s)
+	i := 10
+	var f float32 = float32(i)
+	var d float64 = float64(f)
+	var b bool = bool(true)
+	fmt.Printf("%v,  %v, %v, %v\n", i, f, d, b)
 }
