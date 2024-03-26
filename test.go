d195b1de524842920a1437b921a64b0edf743293
gitover22 <qidizou88@gmail.com>
2024-03-26 10:07:22 +0800

    var type test

--- test.go
+++ test.go
@@ -1,16 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/cmplx"
+)
 
-/**
-*
-*
- */
-var i, j int = 2, 3
+var (
+	tobe    bool       = true
+	MAX_INT uint64     = 1<<64 - 1
+	z       complex128 = cmplx.Sqrt(-5 + 12i)
+)
 
-// k := 3 函数外不能这样使用，必须使用var func
 func main() {
-	var c, python, java = true, false, "string" // 默认为flase
-	k := "fsdaf"
-	fmt.Println(i, j, c, python, java, k)
+	fmt.Printf("type: %T ,value:%v\n", tobe, tobe)
+	fmt.Printf("type: %T ,value:%v\n", MAX_INT, MAX_INT)
+	fmt.Printf("type: %T ,value:%v\n", z, z)
+
 }
