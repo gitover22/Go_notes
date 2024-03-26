fd99ae3f4b96f0112b8ae3ec9f21fd7ee7316329
gitover22 <qidizou88@gmail.com>
2024-03-26 12:27:34 +0800

    if可以添加执行语句的

--- test.go
+++ test.go
@@ -11,6 +11,8 @@ import (
 func __min(x, y, lim float64) float64 {
 	if v := math.Pow(x, y); v < lim { // if后是可以加一个执行语句的
 		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim) //%g能自动选择合适的输出格式
 	}
 	return lim
 }
