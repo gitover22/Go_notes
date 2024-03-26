f4c83b4476a396c0cb69fc123b3409e54b009388
gitover22 <qidizou88@gmail.com>
2024-03-26 18:29:38 +0800

    接口的定义和实现

--- test.go
+++ test.go
@@ -5,24 +5,40 @@ import (
 	"math"
 )
 
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+	var a Abser // 定义一个接口变量，只要实现了该接口的，就可以赋值
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f  // a MyFloat 实现了 Abser
+	a = &v // a *Vertex 实现了 Abser
+
+	// 下面一行，v 是一个 Vertex（而不是 *Vertex）
+	// 所以没有实现 Abser。
+	// a = v
+
+	fmt.Println(a.Abs())
+}
+
+type MyFloat float64
+
+// MyFloat 实现了 Abser接口
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
 type Vertex struct {
 	X, Y float64
 }
 
-// 值接收者只是在对副本进行操作
-func Abs(v Vertex) float64 {
+// *Vertex 实现了 Abser接口
+func (v *Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
-}
-
-// 指针接收者能够改变值
-func Scale(v *Vertex, f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
-}
-
-func main() {
-	v := Vertex{3, 4}
-	Scale(&v, 10)
-	fmt.Println(v.X, v.Y)
-	fmt.Println(Abs(v))
 }
