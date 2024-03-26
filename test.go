f6beed5d994457f7a70e36207c5089e9428169d8
gitover22 <qidizou88@gmail.com>
2024-03-26 12:54:46 +0800

    defer stack test

--- test.go
+++ test.go
@@ -2,22 +2,16 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 /**
 * @brife min(x^y,lim)
  */
 func main() {
-	t := time.Now()
-	fmt.Println(t.Hour())
-	switch { // 等价于switch true
-	case t.Hour() < 12:
-		fmt.Println("morning")
-	case t.Hour() < 17:
-		fmt.Println("afternoon")
-	default:
-		fmt.Println("evening")
+	fmt.Println("begin")
+	defer fmt.Println("end")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i) //压栈
 	}
 
 }
