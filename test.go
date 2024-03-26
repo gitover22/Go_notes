c2fafcf350c692e878cbc205c0386d608ef30b9e
gitover22 <qidizou88@gmail.com>
2024-03-26 20:14:15 +0800

    channel select

--- test.go
+++ test.go
@@ -1,28 +1,23 @@
 package main
 
-import "fmt"
-
-func fibonacci(c, quit chan int) {
-	x, y := 0, 1
-	for {
-		select { // select 会从分支中找到已经准备好的分支执行
-		case c <- x:
-			x, y = y, x+y
-		case <-quit:
-			fmt.Println("quit")
-			return
-		}
-	}
-}
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-	c := make(chan int)
-	quit := make(chan int)
-	go func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println(<-c)
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
 		}
-		quit <- 0
-	}()
-	fibonacci(c, quit)
+	}
 }
