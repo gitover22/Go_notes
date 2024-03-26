c6b13d0e798665655922850c8d29ec29db4284e0
gitover22 <qidizou88@gmail.com>
2024-03-26 09:42:20 +0800

    split func

--- go.mod
+++ go.mod
@@ -1,2 +1,3 @@
 module github.com/gitover22/go_notes
-go 1.18
+
+go 1.22.1
--- test.go
+++ test.go
@@ -4,15 +4,14 @@ import "fmt"
 
 /**
 * @brief mutil return values func test
-*
+*s
  */
 
-func swap(x, y string) (string, string) {
-	return y, x
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
 }
 func main() {
-	x := "world"
-	y := "hello"
-	z, u := swap(x, y)
-	fmt.Printf("%s,%s", z, u)
+	fmt.Println(split(17))
 }
