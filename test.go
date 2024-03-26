680ab95fbb2698d43fd5c40bd82d258df7e24e35
gitover22 <qidizou88@gmail.com>
2024-03-26 15:07:42 +0800

    range _

--- test.go
+++ test.go
@@ -2,13 +2,12 @@ package main
 
 import "fmt"
 
-var pow = []int{1, 2, 4, 8, 16, 32, 64}
-
 func main() {
-	num := []int{1, 3, 5, 3, 6}
-	fmt.Println(num)
-	// range 返回下标和值
-	for i, v := range pow {
-		fmt.Printf("2**%d = %d\n", i, v)
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i) // == 2**i
+	}
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
 	}
 }
