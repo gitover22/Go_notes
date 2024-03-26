7696297f9515e8ca2de3ea7a42697301a007b6a3
gitover22 <qidizou88@gmail.com>
2024-03-26 20:09:54 +0800

    select channel

--- test.go
+++ test.go
@@ -1,22 +1,28 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-func fibonacci(n int, c chan int) {
+func fibonacci(c, quit chan int) {
 	x, y := 0, 1
-	for i := 0; i < n; i++ {
-		c <- x
-		x, y = y, x+y
+	for {
+		select { // select 会从分支中找到已经准备好的分支执行
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
 	}
-	close(c)
 }
 
 func main() {
-	c := make(chan int, 10)
-	go fibonacci(cap(c), c)
-	for i := range c {
-		fmt.Println(i)
-	}
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
 }
