cb9c38cd482c78454e31bde03510b8dd5a238454
gitover22 <qidizou88@gmail.com>
2024-03-26 15:24:16 +0800

    map test

--- test.go
+++ test.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
+type Vertex struct {
+	Lat, Long float64
+}
+
+var m = map[string]Vertex{
+	"Bell Labs": Vertex{
+		40.68433, -74.39967,
+	},
+	"Google": Vertex{
+		37.42202, -122.08408,
+	},
+	"zou": {175, 65},
+}
+
 func main() {
-	pow := make([]int, 10)
-	for i := range pow {
-		pow[i] = 1 << uint(i) // == 2**i
-	}
-	for _, value := range pow {
-		fmt.Printf("%d\n", value)
-	}
+	fmt.Println(m)
 }
