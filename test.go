2f9498f230bd198d3ad25af6a3fa6d8d47d68e33
gitover22 <qidizou88@gmail.com>
2024-03-26 14:21:15 +0800

    slices make func

--- test.go
+++ test.go
@@ -3,10 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var s []int
-	fmt.Println(s, len(s), cap(s))
-	// 切片的零值是nil,且此时切片没有底层数组
-	if s == nil {
-		fmt.Println("nil!")
-	}
+	a := make([]int, 5)
+	printSlice("a", a) // a 5 5 [0,0,0,0,0]
+
+	b := make([]int, 0, 5)
+	printSlice("b", b) // b 0 5 []
+
+	c := b[:2]
+	printSlice("c", c) // c 2 5 [0,0]
+
+	d := c[2:5]
+	printSlice("d", d) // d 3 3 [0,0,0]
+}
+
+func printSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
 }
