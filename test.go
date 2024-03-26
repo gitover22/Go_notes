25aa45e5f3228e91edca45046431eadbb1cb1815
gitover22 <qidizou88@gmail.com>
2024-03-26 16:48:29 +0800

    指针接收者更加实用

--- test.go
+++ test.go
@@ -5,18 +5,24 @@ import (
 	"math"
 )
 
-type MyFloat float64
+type Vertex struct {
+	X, Y float64
+}
 
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
+// 值接收者只是在对副本进行操作
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// 指针接收者能够改变值
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
 }
 
 func main() {
-	fmt.Println(math.Sqrt2)
-	f := MyFloat(-math.Sqrt2)
-	fmt.Println(f)
-	fmt.Println(f.Abs())
+	v := Vertex{3, 4}
+	v.Scale(10)
+	fmt.Println(v.X, v.Y)
+	fmt.Println(v.Abs())
 }
