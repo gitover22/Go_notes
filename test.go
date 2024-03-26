cbdc8a9808936d9b59037013f5a06fb29ab359cf
gitover22 <qidizou88@gmail.com>
2024-03-26 14:43:15 +0800

    range操作

--- test.go
+++ test.go
@@ -1,26 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+var pow = []int{1, 2, 4, 8, 16, 32, 64}
 
 func main() {
-	var s []int
-	printSlice(s)
-
-	// 添加一个空切片
-	s = append(s, 0)
-	printSlice(s)
-
-	// 这个切片会按需增长
-	s = append(s, 1)
-	printSlice(s)
-
-	// 可以一次性添加多个元素
-	s = append(s, 2, 3, 4)
-	printSlice(s)
-}
-
-func printSlice(s []int) {
-	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+	num := []int{1, 3, 5, 3, 6}
+	fmt.Println(num)
+	// range 返回下标和值
+	for i, v := range pow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
 }
