6134d36be51efd8d7498b71a5ef1be93df3b03d2
gitover22 <qidizou88@gmail.com>
2024-03-26 14:11:35 +0800

    slices len and cap

--- test.go
+++ test.go
@@ -2,9 +2,14 @@ package main
 
 import "fmt"
 
+// len():返回切片元素个数
+// cap():从切片第一个元素开始数，到其底层数组元素末尾的个数
 func main() {
 	// 切片默认为从0到尾
 	num := []int{1, 2, 3, 4, 5}
-	aa := num[:]
-	fmt.Println(aa)
+	aa := num[1:2]
+	printSlice(aa)
+}
+func printSlice(s []int) {
+	fmt.Printf("len: %d , casp:%d  v: %v\n", len(s), cap(s), s)
 }
