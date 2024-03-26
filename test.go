bc6ea2283dff6707137b8b10cef64ab100a8550a
gitover22 <qidizou88@gmail.com>
2024-03-26 19:53:57 +0800

    chan 缓冲区

--- test.go
+++ test.go
@@ -1,22 +1,13 @@
 package main
 
-import "fmt"
-
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
-	}
-	c <- sum // 将和送入 c
-}
+import (
+	"fmt"
+)
 
 func main() {
-	s := []int{7, 2, 8, -9, 4, 0}
-
-	c := make(chan int) //信道必须要先创建
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c // 从 c 中接收
-
-	fmt.Println(x, y, x+y)
+	ch := make(chan string, 2)
+	ch <- "aa"
+	ch <- "bb"
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
 }
