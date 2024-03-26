2e08ad95dcc0ce9b2715aed412378d03fb15fd38
gitover22 <qidizou88@gmail.com>
2024-03-26 19:33:33 +0800

    Reader接口

--- test.go
+++ test.go
@@ -2,28 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"io"
+	"strings"
 )
 
-type MyError struct {
-	When time.Time
-	What string
-}
-
-func (e *MyError) Error() string {
-	return fmt.Sprintf("at %v, %s",
-		e.When, e.What)
-}
-
-func run() error {
-	return &MyError{
-		time.Now(),
-		"it didn't work",
-	}
-}
-
 func main() {
-	if err := run(); err != nil {
-		fmt.Println(err)
+	r := strings.NewReader("Hello, Reader!")
+
+	b := make([]byte, 8) // b为数组切片，len为8，cap为8
+	for {
+		n, err := r.Read(b) //填充b，并返回填充的字节数和错误值
+		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
+		fmt.Printf("b[:n] = %q\n", b[:n])
+		if err == io.EOF { // 遇到结尾，返回EOF
+			break
+		}
 	}
 }
