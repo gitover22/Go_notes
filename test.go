b1ec0059ff58dedf65309fc0ba32836f9a9d0621
gitover22 <qidizou88@gmail.com>
2024-03-26 12:32:23 +0800

    switch go

--- test.go
+++ test.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
 
 /**
 * @brife min(x^y,lim)
  */
-func __min(x, y, lim float64) float64 {
-	if v := math.Pow(x, y); v < lim { // if后是可以加一个执行语句的
-		return v
-	} else {
-		fmt.Printf("%g >= %g\n", v, lim) //%g能自动选择合适的输出格式
-	}
-	return lim
-}
 func main() {
-	fmt.Println(__min(3, 2, 10), __min(3, 3, 10))
+	fmt.Print("go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+
+	case "linux":
+		fmt.Println("linux")
+	default:
+		fmt.Printf("%s\n", os)
+	}
 }
