be1b87c492009c620e67c94d6cdaf27ab4dcb428
gitover22 <qidizou88@gmail.com>
2024-03-26 16:33:37 +0800

    Go中结构体的方法

--- test.go
+++ test.go
@@ -2,22 +2,18 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
-// 返回一个“返回int的函数”
-func fibonacci() func() int {
-	a, b := 0, 1 // 斐波那契数列的前两个数
-	return func() int {
-		// 计算下一个斐波那契数
-		next := a
-		a, b = b, a+b
-		return next
-	}
+type Vertex struct {
+	X, Y float64
+}
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
-	f := fibonacci()
-	for i := 0; i < 10; i++ {
-		fmt.Println(f())
-	}
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
 }
