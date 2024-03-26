92b911e60194e923310435b2a5798a9da5d0fcf2
gitover22 <qidizou88@gmail.com>
2024-03-26 10:42:41 +0800

    go的for循环

--- test.go
+++ test.go
@@ -2,21 +2,10 @@ package main
 
 import "fmt"
 
-// 一个未指定类型的常量由上下文来决定其类型
-const (
-	Big   = 1 << 100
-	Small = Big >> 99
-)
-
-func needInt(x int) int {
-	return x*10 + 1
-}
-func needFloat(x float64) float64 {
-	return x * 0.1
-}
 func main() {
-	fmt.Println(needInt(Small))
-	fmt.Println(needFloat(Small))
-	fmt.Println(needFloat(Big))
-	// fmt.Println(needInt(Big)) 报错，Big大于int能表示的最大数
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
 }
