023a5cbe3de2011a115667df8eb38e77d599d90e
gitover22 <qidizou88@gmail.com>
2024-03-26 12:10:35 +0800

    if test

--- test.go
+++ test.go
@@ -5,12 +5,15 @@ import (
 	"math"
 )
 
-func sqrt(x float64) string {
-	if x < 0 {
-		return sqrt(-x) + ""
+/**
+* @brife min(x^y,lim)
+ */
+func __min(x, y, lim float64) float64 {
+	if v := math.Pow(x, y); v < lim { // if后是可以加一个执行语句的
+		return v
 	}
-	return fmt.Sprint(math.Sqrt(x))
+	return lim
 }
 func main() {
-	fmt.Println(sqrt(9), sqrt(-4))
+	fmt.Println(__min(3, 2, 10), __min(3, 3, 10))
 }
