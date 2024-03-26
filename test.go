110328f861bd36dd7210d807a988272474172f78
gitover22 <qidizou88@gmail.com>
2024-03-26 14:13:03 +0800

    slices nil test

--- test.go
+++ test.go
@@ -2,14 +2,11 @@ package main
 
 import "fmt"
 
-// len():返回切片元素个数
-// cap():从切片第一个元素开始数，到其底层数组元素末尾的个数
 func main() {
-	// 切片默认为从0到尾
-	num := []int{1, 2, 3, 4, 5}
-	aa := num[1:2]
-	printSlice(aa)
-}
-func printSlice(s []int) {
-	fmt.Printf("len: %d , casp:%d  v: %v\n", len(s), cap(s), s)
+	var s []int
+	fmt.Println(s, len(s), cap(s))
+	// 切片的零值是nil,且此时切片没有底层数组
+	if s == nil {
+		fmt.Println("nil!")
+	}
 }
