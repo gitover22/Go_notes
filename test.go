268a10fdd59d1c5ff26b3fdaa180ae6f78db0f87
gitover22 <qidizou88@gmail.com>
2024-03-26 13:57:34 +0800

    切片文法

--- test.go
+++ test.go
@@ -3,22 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	names := [5]string{
-		"111",
-		"222",
-		"333",
-		"444",
-		"555",
+	names := []string{"zou", "gan"}
+	fmt.Println(names)
+	s := []struct {
+		x int
+		y bool
+	}{
+		{1, true},
+		{0, false},
 	}
-	fmt.Println(names)
-	var ss []string = names[0:2]
-	var bb []string = names[1:3]
-	fmt.Println(ss)
-	fmt.Println(bb)
-
-	ss[1] = "666" // 通过切片修改值，会影响原值
-	fmt.Println("after change")
-	fmt.Println(ss)
-	fmt.Println(bb)
-	fmt.Println(names)
+	fmt.Println(s)
 }
